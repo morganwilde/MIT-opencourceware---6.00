386a49dfb307ccd685d61515b62b87e4bf225cfb
morganwilde <wilde.morgan@gmail.com>
2013-02-21 14:47:04 +0200

    git auto

---
+++ 1-introduction-to-600/lecture-code-1.py
@@ -0,0 +1,5 @@
+# output
+print 'This is output'
+
+# input
+raw_input('Input is received after the colon: ')
---
+++ 1-introduction-to-600/problem-set-0.py
@@ -0,0 +1,16 @@
+# Problem set 0 # Name: Morgan Wilde # Time spent: 16:00
+
+import sys
+write = sys.stdout.write
+write('20')
+write('05\n')
+
+exit()
+
+#input
+dateOfBirth = raw_input('Enter your date of birth (dd/mm/yy): ')
+lastName = raw_input('Enter your last name: ')
+
+# output
+print '%s' % (dateOfBirth)
+print '%s' % lastName
