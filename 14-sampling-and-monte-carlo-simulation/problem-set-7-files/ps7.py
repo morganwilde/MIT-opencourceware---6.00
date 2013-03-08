961f637e9276200d9ec5023b00fa9547cf4ef447
morganwilde <wilde.morgan@gmail.com>
2013-03-08 16:34:30 +0200

    auto

--- 14-sampling-and-monte-carlo-simulation/problem-set-7-files/ps7.py
+++ 14-sampling-and-monte-carlo-simulation/problem-set-7-files/ps7.py
@@ -146,7 +146,7 @@ class SimplePatient(object):
 #
 # PROBLEM 2
 #
-def simulationWithoutDrug():
+def simulationWithoutDrug(plot = True):
     """
     Run the simulation and plot the graph for problem 2 (no drugs are used,
     viruses do not have any drug resistance).    
@@ -164,19 +164,158 @@ def simulationWithoutDrug():
     # run simulation
     xAxis = range(1, 301)
     yAxis = []
+    yMax = 0
+    growthStops = 0
     for i in range(300):
-        yAxis.append(patient.update())
+        updated = patient.update()
+        if updated > yMax:
+            yMax = updated
+        if len(yAxis) > 40:
+            if yAxis[-1] > updated and\
+               yAxis[-1] > yAxis[-40] and\
+               growthStops == 0:
+                growthStops = i-1
+        yAxis.append(updated)
+
+    if plot == True:
+        # plot it
+        pylab.plot(xAxis, yAxis)
+        pylab.title('Simulating virus growth in a patient over time')
+        pylab.xlabel('Time steps')
+        pylab.ylabel('Virus population')
+        pylab.annotate('Growth stops (step no = '+str(growthStops)+')',\
+                       xy=(growthStops, yAxis[growthStops]),\
+                       xytext=(growthStops+15, yAxis[growthStops]-175),\
+                       arrowprops=dict(facecolor='black', shrink=0.05))
+        pylab.ion()
+        pylab.show()
+    elif plot == 'peak':
+        return growthStops
+    else:
+        return yAxis
+
+def runSimulations(times = 1):
+    """
+    runs the simulationWithoutDrug() simulation times number of times
+    """
+    simulationResults = []
+    fileData = open('plot-data-'+str(times)+'.txt', 'a')
+    for time in range(times):
+        simulationResults.append( simulationWithoutDrug(False)[-1] )
+        fileData.write(str(simulationWithoutDrug(False)[-1]) + '\n')
 
     # plot it
-    pylab.plot(xAxis, yAxis)
-    pylab.title('Simulating virus growth in a patient over time')
-    pylab.xlabel('Time steps')
-    pylab.ylabel('Virus population')
+    pylab.plot(simulationResults, 'bo')
+    pylab.title('Final population after simulation')
+    pylab.xlabel('Simulation number')
+    pylab.ylabel('Final virus population')
+    pylab.ion()
+    pylab.show()
+
+def runSimulationsPeak(times = 1):
+    """
+    runs times times to average the peak
+    """
+    simulationResults = []
+    fileData = open('plot-data-peaks-'+str(times)+'.txt', 'a')
+    for time in range(times):
+        simulationResults.append( simulationWithoutDrug('peak') )
+        fileData.write(str(simulationWithoutDrug('peak')) + '\n')
+
+    # plot it
+    print simulationResults
+    pylab.plot(simulationResults, 'bo')
+    pylab.title('How long does it take before population stops growing?')
+    pylab.xlabel('Simulation number')
+    pylab.ylabel('Steps before growth stopped')
     pylab.ion()
     pylab.show()
 
 #
 # Test harness
 #
+def stdDev(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    return (tot/len(X))**0.5
+
 if __name__ == '__main__':
     simulationWithoutDrug()
+    # Collect sample
+    #runSimulations(1)
+    #runSimulations(10)
+    #runSimulations(100)
+    #runSimulations(1000)
+    # Collect peak samples
+##    runSimulationsPeak(1)
+##    runSimulationsPeak(10)
+##    runSimulationsPeak(100)
+##    runSimulationsPeak(1000)
+
+    # Find min/max for samples
+##    samples = [1, 10, 100, 1000]
+##    minmaxDict = {}
+##    for sample in samples:
+##        minmaxDict[str(sample)+'-min'] = 0
+##        minmaxDict[str(sample)+'-max'] = 0
+##        minmaxDict[str(sample)+'-mean'] = 0
+##        minmaxDict[str(sample)+'-list'] = []
+##        sums = 0
+##        fileData = open('plot-data-'+str(sample)+'.txt', 'r')
+##        for line in fileData:
+##            result = int(line)
+##            minmaxDict[str(sample)+'-list'].append(result)
+##            sums += result
+##            if result < minmaxDict[str(sample)+'-min'] or minmaxDict[str(sample)+'-min'] == 0:
+##                minmaxDict[str(sample)+'-min'] = result
+##            if result > minmaxDict[str(sample)+'-max']:
+##                minmaxDict[str(sample)+'-max'] = result
+##        # find mean
+##        minmaxDict[str(sample)+'-mean'] = sums/sample
+
+    # Find growth stop from samples
+##    samples = [1, 10, 100, 1000]
+##    minmaxDict = {}
+##    for sample in samples:
+##        minmaxDict[str(sample)+'-mean'] = 0
+##        minmaxDict[str(sample)+'-list'] = []
+##        sums = 0
+##        fileData = open('plot-data-peaks-'+str(sample)+'.txt', 'r')
+##        for line in fileData:
+##            result = int(line)
+##            minmaxDict[str(sample)+'-list'].append(result)
+##            sums += result
+##        # find mean
+##        minmaxDict[str(sample)+'-mean'] = sums/sample
+    print '1000 mean:', minmaxDict['1000-mean']
+    print 'standard deviation of 1000:', stdDev(minmaxDict['1000-list'])
+
+    # Print results
+##    print '1 min:', minmaxDict['1-min']
+##    print '1 max:', minmaxDict['1-max']
+##    print '1 mean:', minmaxDict['1-mean']
+##    print '10 min:', minmaxDict['10-min']
+##    print '10 max:', minmaxDict['10-max']
+##    print '10 mean:', minmaxDict['10-mean']
+##    print 'standard deviation of 10:', stdDev(minmaxDict['10-list'])
+##    print '100 min:', minmaxDict['100-min']
+##    print '100 max:', minmaxDict['100-max']
+##    print '100 mean:', minmaxDict['100-mean']
+##    print 'standard deviation of 100:', stdDev(minmaxDict['100-list'])
+##    print '1000 min:', minmaxDict['1000-min']
+##    print '1000 max:', minmaxDict['1000-max']
+##    print '1000 mean:', minmaxDict['1000-mean']
+##    print 'standard deviation of 1000:', stdDev(minmaxDict['1000-list'])
+
+    # Plot results
+##    pylab.hist(minmaxDict['1000-list'], bins = 20)
+##    pylab.hist(minmaxDict['100-list'], bins = 20)
+##    pylab.hist(minmaxDict['10-list'], bins = 20)
+##    pylab.title('Final virus populations after 10, 100, 100 simulations')
+##    pylab.xlabel('Numer of viruses at the end')
+##    pylab.ylabel('How many times it appears')
+##    pylab.ion()
+##    pylab.show()
+    pass
---
+++ 14-sampling-and-monte-carlo-simulation/problem-set-7-files/writeup.md
@@ -0,0 +1,14 @@
+Problem Set 7 Write-up
+======================
+
+### How long it takes for a population to stop growing
+
+After running `1000` tests, the mean amount of steps before the population stops growing is - `53` (with the standard deviation of `3.73614761486`).
+
+Below is a sample illustrating a single simulation of growth:
+
+![single simulation](http://dl.dropbox.com/u/31042440/mit-ocw-600/unit-2/virus-growth-example.png)
+
+Here is the distribution of the number of steps before growth stops:
+
+![growth stops distribution](http://dl.dropbox.com/u/31042440/mit-ocw-600/unit-2/virus-growth-peak-distribution.png)
